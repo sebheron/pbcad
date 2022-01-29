b90177032bdefaf5372cc1dd90a92611cf245139
Seb Heron <sebheron21@hotmail.co.uk>
2022-01-29 00:30:42 +0000

    Began console output display

--- src/main/java/com/pb/pbcad/PbcadController.java
+++ src/main/java/com/pb/pbcad/PbcadController.java
@@ -1,13 +1,15 @@
 package com.pb.pbcad;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PbcadController {
 
     @RequestMapping("/")
-    public String Test()
+    public String Display(Model model)
     {
-        return "index";
+        model.addAttribute("consoleOutput", "This is the console output!");
+        return "display";
     }
 }
