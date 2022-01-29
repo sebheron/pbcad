3e8e8a01f06ba7d717711f3a95cfc7087ce22052
Seb Heron <sebheron21@hotmail.co.uk>
2022-01-29 00:33:51 +0000

    Switch to getMapping

--- src/main/java/com/pb/pbcad/PbcadController.java
+++ src/main/java/com/pb/pbcad/PbcadController.java
@@ -1,12 +1,12 @@
 package com.pb.pbcad;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PbcadController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String Display(Model model)
     {
         model.addAttribute("consoleOutput", "This is the console output!");
