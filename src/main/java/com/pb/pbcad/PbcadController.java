9e5146a112f4538bff8b49eb64621afe28883f2f
Seb Heron <sebheron21@hotmail.co.uk>
2022-01-29 16:14:10 +0000

    add attribute

--- src/main/java/com/pb/pbcad/PbcadController.java
+++ src/main/java/com/pb/pbcad/PbcadController.java
@@ -1,14 +1,15 @@
 package com.pb.pbcad;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PbcadController {
 
     @GetMapping("/")
-    public String Test()
+    public String Test(Model model)
     {
+        model.addAttribute("consoleLog", "CONSOLE LOG INFORMATION");
         return "index";
     }
 }
