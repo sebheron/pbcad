19e476f22c0ca755b3f1b4dabec485a31e3e9401
Seb Heron <sebheron21@hotmail.co.uk>
2022-02-11 17:39:52 +0000

    Navigation addition

--- src/main/java/com/pb/pbcad/PbcadController.java
+++ src/main/java/com/pb/pbcad/PbcadController.java
@@ -2,13 +2,15 @@ package com.pb.pbcad;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PbcadController {
 
     @GetMapping("/")
-    public String Test(Model model)
+    public String Test(@RequestParam(name="ds", required=false, defaultValue="") String ds, Model model)
     {
+        model.addAttribute("ds", ds);
         model.addAttribute("log", "CONSOLE LOG INFORMATION");
         return "index";
     }
