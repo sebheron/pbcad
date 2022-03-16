2267138493863ada030e23609fd93473387b5dfb
Seb Heron <sebheron21@hotmail.co.uk>
2022-03-16 10:45:59 +0000

    Setup service and auto wiring

--- src/main/java/com/pb/pbcad/PbcadController.java
+++ src/main/java/com/pb/pbcad/PbcadController.java
@@ -1,4 +1,5 @@
 package com.pb.pbcad;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +8,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class PbcadController {
 
+    private final PbcadService pbcadService;
+
+    @Autowired
+    public PbcadController(PbcadService pbcadService) {
+        this.pbcadService = pbcadService;
+    }
+
     @GetMapping("/")
     public String Test(@RequestParam(name="ds", required=false, defaultValue="") String ds, Model model)
     {
         model.addAttribute("ds", ds);
-        model.addAttribute("log", "CONSOLE LOG INFORMATION");
+        model.addAttribute("log", this.pbcadService.InterpretDisplayString(ds));
         return "index";
     }
 }
---
+++ src/main/java/com/pb/pbcad/PbcadService.java
@@ -0,0 +1,48 @@
+package com.pb.pbcad;
+
+import org.sbml.jsbml.SBMLDocument;
+import org.sbml.jsbml.SBMLWriter;
+import org.sbolstandard.core2.SBOLDocument;
+import org.sbolstandard.core2.SBOLValidationException;
+import org.springframework.stereotype.Repository;
+import org.virtualparts.VPRException;
+import org.virtualparts.sbol.SVPWriteHandler;
+import org.virtualparts.ws.client.VPRWebServiceClient;
+
+import javax.ws.rs.client.WebTarget;
+import javax.xml.stream.XMLStreamException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Repository
+public class PbcadService {
+    public String InterpretDisplayString(String displayString) {
+        if (displayString.equals("")) return "Output";
+
+        try {
+            SBOLDocument sbolDesign = SVPWriteHandler.convertToSBOL(displayString, "tu");
+            WebTarget target = VPRWebServiceClient.getVPRWebServiceTarget("https://virtualparts.org/virtualparts-ws/webapi/model/svpwrite");
+            SBMLDocument sbmlDoc = VPRWebServiceClient.getModel(target, sbolDesign);
+            SBMLWriter.write(sbmlDoc, "cad.xml", ' ', (short) 2);
+            return Files.readString(Path.of("cad.xml"));
+        }
+        catch (SBOLValidationException e) {
+            return "Error occurred with SBOL validation, check design string.";
+        }
+        catch (VPRException e) {
+            return "Error occurred with VPR, check design string.";
+        }
+        catch (XMLStreamException e) {
+            return "Error occurred with VPR in SBOL to SBML conversion.";
+        }
+        catch (FileNotFoundException e) {
+            return "SBML File error occurred.";
+        }
+        catch (IOException e) {
+            return "SBML File error occurred.";
+        }
+    }
+}
