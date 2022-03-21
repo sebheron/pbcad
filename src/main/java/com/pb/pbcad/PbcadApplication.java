c5bf0518f01efed542c1e4fb6c0eccba75a573e9
Seb Heron <sebheron21@hotmail.co.uk>
2022-03-21 16:25:30 +0000

    Component scan added

--- src/main/java/com/pb/pbcad/PbcadApplication.java
+++ src/main/java/com/pb/pbcad/PbcadApplication.java
@@ -2,8 +2,10 @@ package com.pb.pbcad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({ "com.pb.pbcad.*" })
 public class PbcadApplication {
     public static void main(String[] args) {
         SpringApplication.run(PbcadApplication.class, args);
--- src/main/java/com/pb/pbcad/PbcadController.java
+++ src/main/java/com/pb/pbcad/PbcadController.java
@@ -1,4 +1,5 @@
 package com.pb.pbcad;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
