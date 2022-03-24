b2b95e2a08ba1295aa8a7fe036ac97f77acd9a5c
Seb Heron <sebheron21@hotmail.co.uk>
2022-03-24 03:59:55 +0000

    Level out exceptions

--- src/main/java/com/pb/pbcad/PbcadService.java
+++ src/main/java/com/pb/pbcad/PbcadService.java
@@ -3,16 +3,12 @@ package com.pb.pbcad;
 import org.sbml.jsbml.SBMLDocument;
 import org.sbml.jsbml.SBMLWriter;
 import org.sbolstandard.core2.SBOLDocument;
-import org.sbolstandard.core2.SBOLValidationException;
 import org.springframework.stereotype.Repository;
-import org.virtualparts.VPRException;
 import org.virtualparts.sbol.SVPWriteHandler;
 import org.virtualparts.ws.client.VPRWebServiceClient;
 
 import javax.ws.rs.client.WebTarget;
-import javax.xml.stream.XMLStreamException;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -28,17 +24,8 @@ public class PbcadService {
             SBMLWriter.write(sbmlDoc, "cad.xml", ' ', (short) 2);
             return Files.readString(Path.of("cad.xml"));
         }
-        catch (SBOLValidationException e) {
-            return "Error occurred with SBOL validation, check design string.";
-        }
-        catch (VPRException e) {
-            return "Error occurred with VPR, check design string.";
-        }
-        catch (XMLStreamException e) {
-            return "Error occurred with VPR in SBOL to SBML conversion.";
-        }
-        catch (IOException e) {
-            return "SBML File error occurred.";
+        catch (Exception e) {
+            return "Error occurred, check design string.";
         }
     }
 }
